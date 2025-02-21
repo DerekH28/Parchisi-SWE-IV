d94507a052dcd56fc7fd01316e73a61849ed99fe
Kenny Morales <knnymrls@outlook.com>
2025-02-21 15:58:16 -0600

    Changed structure

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import DiceRoller from "./components/DiceRoller";
+
+function App() {
+  return (
+    <div>
+      <h1>WebSocket Counter</h1>
+      <DiceRoller />
+    </div>
+  );
+}
+
+export default App;
