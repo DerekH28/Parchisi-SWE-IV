24c7e1b999d606b85b8a027a5ca3f3d408137462
Kenny Morales <knnymrls@outlook.com>
2025-05-04 19:54:08 -0500

    Small error

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
 import Login from "./pages/Login";
 import GamePage from "./pages/GamePage";
 import { AuthProvider } from "./context/AuthProvider";
